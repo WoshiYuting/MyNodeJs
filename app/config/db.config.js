8f34964d00b7f00da30789da8c5fc9e027095959
WoshiYuting <pand3224@gmail.com>
2022-08-22 20:53:13 +0700

    db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,12 +1,20 @@
 const mysql = require("mysql")
 
+// const db = mysql.createConnection({
+//     // host : "localhost",
+//     host : '127.0.0.1',
+//     database : "nodejs",
+//     user : "root",
+//     password : "3MLzX7X24gWrSb4B",
+//     port : "3307"
+// });
 const db = mysql.createConnection({
     // host : "localhost",
-    host : '127.0.0.1',
-    database : "nodejs",
-    user : "root",
-    password : "3MLzX7X24gWrSb4B",
-    port : "3307"
+    host : 'bwdcixpqreutittem59u-mysql.services.clever-cloud.com',
+    database : "bwdcixpqreutittem59u",
+    user : "uz5y72c5ntnbakpy",
+    password : "vfY9gh6gemwW9nshkFTw",
+    port : "3306"
 });
 // const db = mysql.createConnection({
 //     host : "localhost",
